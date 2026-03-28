4fa0c8d2b64fa61b82d34b08b32a3f243b23a279
techjoe57 <neequayejesse@gmail.com>
2026-03-28 12:59:26 +0000

    Create application entry point

--- backend/app.py
+++ backend/app.py
@@ -10,15 +10,32 @@ INSTRUCTIONS:
 """
 
 # TODO: import Flask
+from flask import Flask
+from extensions import db
+
 # TODO: import config settings
+from config import Config
 # TODO: import route blueprints
 
 # TODO: initialize Flask app
+app = Flask(__name__)
 
 # TODO: load configurations from config.py
-
+app.config.from_object(Config)
 # TODO: initialize database (SQLAlchemy)
+db.init_app(app)
 
+from models import User
+
+with app.app_context():
+     db.create_all()
 # TODO: register routes (e.g., auth routes)
+@app.route('/')
+def index():
+    return "Hello World!!"
+
+print(app.config["SQLALCHEMY_DATABASE_URI"])
 
 # TODO: run the app (debug mode for development)
+if __name__ == '__main__':
+    app.run(debug=True)
