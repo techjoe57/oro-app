e0e1797f45a272be843b42823d1c3d7f64d13c48
techjoe57 <neequayejesse@gmail.com>
2026-03-28 18:55:20 +0000

    Fix hashed password check error

--- backend/models.py
+++ backend/models.py
@@ -33,7 +33,7 @@ class User(db.Model):
 
     def check_password(self, password):
         """Checks the provided password against the stored hash"""
-        return check_password_hash(self.password_hash, password) # type: ignore
+        return check_password_hash(self.password_hash, password)
     
     def __repr__(self):
         return f"<User {self.username}>"
