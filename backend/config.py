eb05df4ef682680d5ff662b66a2902c54b86c8de
techjoe57 <neequayejesse@gmail.com>
2026-03-28 11:17:16 +0000

    Create application configuration file

--- backend/config.py
+++ backend/config.py
@@ -8,8 +8,19 @@ INSTRUCTIONS:
 """
 
 # TODO: import os
+import os
+
 # TODO: load .env file
+from dotenv import load_dotenv
+load_dotenv()
 
 # TODO: create Config class
 # Example:
 # DB_URI = "mysql+pymysql://user:password@localhost/db_name"
+class Config:
+    DB_USER = os.environ.get("DB_USER")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_NAME = os.environ.get("DB_NAME")
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
