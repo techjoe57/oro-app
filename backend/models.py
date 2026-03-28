87ee73d4de2a8e22e1d64fef1cef161d178cb8fa
techjoe57 <neequayejesse@gmail.com>
2026-03-28 10:54:03 +0000

    Create models

--- backend/models.py
+++ backend/models.py
@@ -9,6 +9,8 @@ INSTRUCTIONS:
 """
 
 # TODO: import SQLAlchemy instance
+from extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # TODO: create User model
 # Fields:
@@ -16,5 +18,24 @@ INSTRUCTIONS:
 # - username (string)
 # - email (unique)
 # - password (hashed)
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    # Hash plain text passwords
+    def set_password(self, password):
+        """Hashes the password and sets the hashed password column"""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Checks the provided password against the stored hash"""
+        return check_password_hash(self.password_hash, password) # type: ignore
+    
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 # TODO: create relationships if needed
